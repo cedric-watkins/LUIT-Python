78d759813933c4dde49592182ea21fa5caf40914
cedric-watkins <ced.wat1@gmail.com>
2022-09-30 01:48:38 +0000

    Modified code for temp calculator

--- Lab:Fahrenheit-Celsius.py
+++ Lab:Fahrenheit-Celsius.py
@@ -1,6 +1,6 @@
 #This Code will Convert Fahrenheit(User input) to Celsius.
 
-F = float(input('What temperature(in Fahrenheit) would you like converted(to Celsius? '))
-C = float((F - 32) * 5 / 9)
+F = float(input("What temperature(in Fahrenheit) would you like converted(to Celsius)? "))
+C = (F - 32) * 5 / 9
 print(F, "F is" , round(C,2) , "C")
 
