5c7715a6eee319142a513de10504b5c24d56ab3d
cedric-watkins <ced.wat1@gmail.com>
2022-10-02 03:29:52 +0000

    Created a game for listing AWS Services

--- AWS-Services.py
+++ AWS-Services.py
@@ -1,9 +1,9 @@
-#This project will allow users to create a list of AWS Services through user input.
-#It will randomly delete 2 items from the list, and see if the user can guess which items where removed
+#This project will allow players to create a list of AWS Services through user input.
+#It will randomly delete 2 items from the list, and see if the player can guess which items where removed
 
 #Request the user to Enter AWS Services into a list
 services = []
-print("!!!Let's make a list of AWS Services!!! \n")
+print("!!!LET'S PLAY A GAME!!! \n???How many AWS services do you know??? \n\n!!!Let's build a list and make a game of it!!!")
 
 while True:
     services.append(input("Enter an AWS Service: ").title())
