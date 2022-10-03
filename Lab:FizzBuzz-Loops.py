06d2f49a5c391f987e26707d3dd3bafca14c5f4f
cedric-watkins <ced.wat1@gmail.com>
2022-10-03 03:41:35 +0000

    Used loop to determine if a range of number are divisible by 3 and/or 5

---
+++ Lab:FizzBuzz-Loops.py
@@ -0,0 +1,13 @@
+my_range = int(input("How many number should we process? "))
+
+for i in range(1, my_range + 1):
+    
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+        
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
